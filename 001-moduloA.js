ee1fc68809b5b2e4b9f9fee186160b3f48fc5bdf
Antonio Jonilei <100094268+antoniojonilei@users.noreply.github.com>
2022-09-15 15:34:07 -0300

    modulo

---
+++ 001-moduloA.js
@@ -0,0 +1,6 @@
+// Em NODE um arquivo representa um módulo
+// Tudo que se escreve em módulo, fica visivel somente no módulo
+
+this.ola = 'Faça Pessoal'
+exports.bemVindo = 'Bem vindo ao node'
+module.exports.ateLogo = 'Até o próximo exemplo'

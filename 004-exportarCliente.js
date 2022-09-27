594ec5d36136ff4fcc737cb040b1c13f62ae5a7a
Antonio Jonilei <100094268+antoniojonilei@users.noreply.github.com>
2022-09-27 16:40:47 -0300

    json

--- 004-exportarCliente.js
+++ 004-exportarCliente.js
@@ -1,2 +1,3 @@
 const exportar = require('./004-exportar')
 console.log(exportar)
+console.log(module.exports)
---
+++ funcionarios/funcionarios.js
@@ -0,0 +1,23 @@
+// http://files.cod3r.com.br/curso-js/funcionarios.json
+
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+const chineses = func => func.pais === 'China'
+const mulheres = func => func.genero === 'F'
+const menorSalario = (func, funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual
+}
+
+axios.get(url).then(response => {
+    const funcionarios = response.data
+    console.log(funcionarios)
+
+    // mulher chinesa com menor salario
+    const func = funcionarios
+        .filter(chineses)
+        .filter(mulheres)
+        .reduce(menorSalario)
+
+    console.log(func)
+})
